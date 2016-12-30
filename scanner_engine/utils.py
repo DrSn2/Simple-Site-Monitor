c657d1b1635f9cb244dac4ca499b07702458f72e
Josowsky <bartosz@jozwowiak.pl>
2016-12-30 17:44:31 +0100

    Get data from request response FIX

--- scanner_engine/utils.py
+++ scanner_engine/utils.py
@@ -3,18 +3,20 @@ from bs4 import BeautifulSoup
 
 def get_title(html):
     soup = BeautifulSoup(html)
-    title = soup.title.string.strip() if soup.title else ''
+    title = soup.title
+    title = title.string.strip() if title else ''
     return title
 
 
 def get_description(html):
     soup = BeautifulSoup(html)
-    description = soup.find(attrs={"name": "description"}).attrs['content'] if soup.find(
-        attrs={"name": "description"}) else ''
+    description = soup.find(attrs={"name": "description"})
+    description = description.attrs['content'] if description else ''
     return description
 
 
 def get_h1(html):
     soup = BeautifulSoup(html)
-    h1 = soup.h1.string if soup.h1.string else ''
+    h1 = soup.find('h1')
+    h1 = h1.string.strip() if h1 else ''
     return h1
